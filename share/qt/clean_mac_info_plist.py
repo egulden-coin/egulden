72649d289f4532e03dad8e0d0aa8d35befdc76bf
Curve25519 <cybersec05887071+curve@gmail.com>
2014-04-12 11:28:49 +0200

    Update clean_mac_info_plist.py

--- share/qt/clean_mac_info_plist.py
+++ share/qt/clean_mac_info_plist.py
@@ -9,7 +9,7 @@ from datetime import date
 bitcoinDir = "./";
 
 inFile     = bitcoinDir+"/share/qt/Info.plist"
-outFile    = "Litecoin-Qt.app/Contents/Info.plist"
+outFile    = "EGulden-Qt.app/Contents/Info.plist"
 version    = "unknown";
 
 fileForGrabbingVersion = bitcoinDir+"bitcoin-qt.pro"
